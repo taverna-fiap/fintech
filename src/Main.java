7e8c930d2578306f805cbed6a319303c032cb175
Gustavo <siqgustavo2@hotmail.com>
2025-02-27 01:24:37 -0300

    Refactor main imports

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import Models.*;
 
 public class Main {
 
@@ -6,14 +6,14 @@ public class Main {
         Person toPerson = new Person("Teste", "teste@teste.com", "000", "000", "teste", "000");
         Account toAccount = new Account ("1", "1111", "1111", "1111", "x");
         Card toCard = new Card  ("1", "Teste", "111", "111", "11/1111", "11/11/1111");
-        Type toType = new Type ("1", "Teste");
+        Tag toTag = new Tag("1", "Teste");
         Category toCategory = new Category("1", "Teste");
         Transaction toTransaction = new Transaction("1", "Teste", "200", "11/11/1111", true);
 
         System.out.println(toPerson.getName());
         System.out.println(toAccount.getAccountId());
         System.out.println(toCard.getCardId());
-        System.out.println(toType.getTypeName());
+        System.out.println(toTag.getTagName());
         System.out.println(toCategory.getCategoryId());
         System.out.println(toTransaction.getValue());
     }
